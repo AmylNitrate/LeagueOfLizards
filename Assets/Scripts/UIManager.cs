ae8ef0149628c49d186b38302ab84396ba2bcb55
Amy Lopes <111661@student.sae.edu.au>
2016-02-23 14:46:28 +1100

    Added New Scenes & Navigation

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+
+    public void GoToLevel(string Level)
+    {
+        SceneManager.LoadScene(Level);
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+
+	}
+	
+	// Update is called once per frame
+      
+	void Update ()
+    {
+
+	}
+}
