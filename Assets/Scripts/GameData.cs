821a5f7a275ea9769e2c571163e3f567f16088ad
Best Guy Matt <matt.batchelor1@gmail.com>
2016-07-11 13:23:36 +1000

    Added in minigame tracking object and commented MiniGameTracker.cs and GameData.cs

--- Assets/Scripts/GameData.cs
+++ Assets/Scripts/GameData.cs
@@ -1,14 +1,32 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
+using System.Collections.Generic;
 using GooglePlayGames;
 using GooglePlayGames.BasicApi.Multiplayer;
 
 public class GameData : MonoBehaviour {
 
+    //Singleton
     public static GameData instance;
 
+    //The types of rounds that players can choose
+    public enum RoundTypes { Assess, Escalate, Fight, RunAway }
+
+    //A dictionary that assigns a numerical value to the round types
+    public Dictionary<RoundTypes, int> roundValues = new Dictionary<RoundTypes, int>();
+
+    //The enemy participant from the GPGS multiplayer services
     public Participant enemyParticipant;
+
+    //Tracking the RHP of both players
+    int myCurrentRHP, enemyCurrentRHP;
+
+    //Checking the number of rounds completed so that players can not do too many assess or escalation rounds
+    int assessRoundsComplete, escalationRoundsComplete;
+
+    //Tracks the current round that the game is on so that the players can't choose to do a round with a smaller value
+    int currentRoundValue;
 
     void Awake()
     {
@@ -19,18 +37,111 @@ public class GameData : MonoBehaviour {
         }
         else
         {
-            Destroy(this.gameObject);
+            DestroyImmediate(this.gameObject);
         }
     }
 
     void Start()
     {
-        foreach(Participant part in MultiplayerController.Instance.GetAllPlayers())
+        GetData();
+    }
+
+    /// <summary>
+    /// Retrieves data from MultiplayerController and the save file, performed on scene load
+    /// </summary>
+    void GetData()
+    {
+        //Gets round info for the dictionary
+        PopDictionary();
+        //Getting the participant ID of the player's opponent
+        foreach (Participant part in MultiplayerController.Instance.GetAllPlayers())
         {
             if (part.ParticipantId != MultiplayerController.Instance.GetParticipantID())
             {
                 enemyParticipant = part;
             }
+        }
+        //Read from save file
+        //myCurrentRHP = 
+        //enemyCurrentRHP = 
+        //Set current round
+        currentRoundValue = roundValues[RoundTypes.Assess];
+    }
+
+    /// <summary>
+    /// Performs clean up end game tasks such as saving and destroying the current game tracking object
+    /// Should be the last thing completed
+    /// </summary>
+    void EndGame()
+    {
+        Destroy(this.gameObject);
+    }
+
+    /// <summary>
+    /// Check if the players are allowed to do another type of a limited round or the game has already passed
+    /// </summary>
+    /// <param name="round">Which round the game is currently on</param>
+    /// <returns></returns>
+    public bool CheckRoundAvailability(RoundTypes round)
+    {
+        if (round == RoundTypes.Assess && currentRoundValue == roundValues[RoundTypes.Assess])
+        {
+            if (assessRoundsComplete < 2)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        else if (round == RoundTypes.Escalate && currentRoundValue <= roundValues[RoundTypes.Escalate])
+        {
+            if (escalationRoundsComplete == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        else
+        {
+            return true;
+        }
+    }
+
+    //Populates the dictionary with the RoundTypes and int values
+    void PopDictionary()
+    {
+        roundValues.Add(RoundTypes.Assess, 0);
+        roundValues.Add(RoundTypes.Escalate, 1);
+        roundValues.Add(RoundTypes.Fight, 2);
+        roundValues.Add(RoundTypes.RunAway, 3);
+    }
+
+    /// <summary>
+    /// Performed at the end of each round to update any necessary information
+    /// </summary>
+    /// <param name="round">Which type of round just ended</param>
+    public void RoundOver(RoundTypes round)
+    {
+        //Update both players RHP
+        switch (round)
+        {
+            case RoundTypes.Assess:
+                assessRoundsComplete++;
+                break;
+            case RoundTypes.Escalate:
+                escalationRoundsComplete++;
+                break;
+            case RoundTypes.Fight:
+                EndGame();
+                break;
+            case RoundTypes.RunAway:
+                EndGame();
+                break;
         }
     }
 }
---
+++ Assets/Scripts/MiniGameTracker.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MiniGameTracker : MonoBehaviour {
+
+    //Singleton reference object
+    public static MiniGameTracker instance;
+
+    //The two players in the game
+    public enum Players { localPlayer, enemy }
+
+    //Player values showing the winners
+    Players assessOneWinner, assessTwoWinner, escalationWinner, fightWinner;
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+    /// <summary>
+    /// Sets the winner of the assessment round
+    /// </summary>
+    /// <param name="winner">The winner of the round</param>
+    /// <param name="round">Checks which of the assess rounds it is. Retrieve from GameData</param>
+    public void SetAssessWinner(Players winner, int round)
+    {
+        if (round == 1)
+        {
+            assessOneWinner = winner;
+        }
+        if (round == 2)
+        {
+            assessTwoWinner = winner;
+        }
+    }
+
+    /// <summary>
+    /// Sets the escalation winner
+    /// </summary>
+    /// <param name="winner">The player who won the round</param>
+    public void SetEscalationWinner(Players winner)
+    {
+        escalationWinner = winner;
+    }
+
+    /// <summary>
+    /// Sets the winner of the fight
+    /// </summary>
+    /// <param name="winner">The player who won the round</param>
+    public void SetFightWinner(Players winner)
+    {
+        fightWinner = winner;
+    }
+}
